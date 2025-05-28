3ed374851885188d28a265275ec9f179ecc7367e
Seu Nome <seu-email@example.com>
2025-05-28 20:22:40 -0300

    fix: habilita server actions no next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
     // Desabilitar a verificação de tipos para o Typescript durante o build
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: true,
+  },
   webpack: (config) => {
     // Resolver problema com mercadopago
     config.resolve.fallback = { 
